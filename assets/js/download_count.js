18539c55688c99a815a51822fd28fc0e8d913477
Sterbenover <904428533@qq.com>
2025-02-13 11:18:37 +0800

    test

---
+++ assets/js/download_count.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  var downloadLink = document.getElementById('download-link');
+  var downloadCountElement = document.getElementById('download-count');
+
+  // 模拟的下载次数，实际上你可以从后端获取
+  var downloadCount = localStorage.getItem('downloadClickCount') || 0;
+
+  if (downloadLink) {
+    downloadLink.addEventListener('click', function (event) {
+      // 发送 Google Analytics 事件
+      gtag('event', 'download', {
+        'event_category': 'file_download',
+        'event_label': 'Drone-Racing-Video',  // 根据实际文件名动态生成
+      });
+      // 更新下载次数
+      downloadCount++;
+      // 更新 localStorage 中的点击次数
+      localStorage.setItem('downloadClickCount', downloadCount);
+      // 更新页面上的点击次数显示
+      downloadCountElement.textContent = "下载次数: " + downloadCount;
+    });
+  }
+});
