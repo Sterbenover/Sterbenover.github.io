431f09ad4b8c432b07824dd3c1b30253065faa8c
Sterbenover <904428533@qq.com>
2025-02-13 11:47:43 +0800

    test

--- assets/js/download_count.js
+++ assets/js/download_count.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // downloadCountElement.textContent = "下载次数: " + downloadCount;
   if (downloadLink) {
     // downloadCountElement.textContent = "下载次数: " + downloadCount;
-    downloadCountElement.textContent = downloadCount;
+    downloadCountElement.textContent = "Download Count: " + downloadCount;
     downloadLink.addEventListener('click', function (event) {
       // 发送 Google Analytics 事件
       gtag('event', 'download', {
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
       localStorage.setItem('downloadClickCount', downloadCount);
       // 更新页面上的点击次数显示
       // downloadCountElement.textContent = "下载次数: " + downloadCount;
-      downloadCountElement.textContent = downloadCount;
+      downloadCountElement.textContent = "Download Count: " + downloadCount;
     });
   }
 });
